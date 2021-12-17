5223db09cde9e587c6b874209a5a9425493ebbc7
Георгий Исаев <georgiyelbaf@gmail.com>
2021-12-18 01:00:23 +0300

    Lab05 fix - три основных функции

--- Labs/Lab05/lab05_fun.py
+++ Labs/Lab05/lab05_fun.py
@@ -10,43 +10,59 @@
 import time
 from random import randint
 
-igrok1 = input('Введите имя 1-го играющего: ')
-igrok2 = input('Введите имя 2-го играющего: ')
 
-countPlaer01 = 0
-countPlaer02 = 0
+def inputPlaer():
+    igrok1 = input('Введите имя 1-го играющего: ')
+    igrok2 = input('Введите имя 2-го играющего: ')
+    return igrok1, igrok2
+
+
+igrok1, igrok2 = inputPlaer()
+
 
 # ЗАДАЧА 01: Добавьте в программу цикл for, реализующий 5 партий (бросков):
-for i in range(5):
-    # Моделирование бросания кубика первым играющим
-    print('Кубик бросает', igrok1)
-    time.sleep(2)
-    n1 = randint(1, 6)
-    print('Выпало:', n1)
-    # Моделирование бросания кубика вторым играющим
-    print('Кубик бросает', igrok2)
-    time.sleep(2)
-    n2 = randint(1, 6)
-    print('Выпало:', n2)
-    # Определение результата (3 возможных варианта)
-    if n1 > n2:
-        print('\tВыиграл', igrok1)
-        countPlaer01 += 1
-    elif n1 < n2:
-        print('\tВыиграл', igrok2)
-        countPlaer02 += 1
-    else:
-        print('\tНичья')
+def corGames():
+    countPlayer01 = 0
+    countPlayer02 = 0
+    for i in range(5):
+        # Моделирование бросания кубика первым играющим
+        print('Кубик бросает', igrok1)
+        time.sleep(2)
+        n1 = randint(1, 6)
+        print('Выпало:', n1)
+        # Моделирование бросания кубика вторым играющим
+        print('Кубик бросает', igrok2)
+        time.sleep(2)
+        n2 = randint(1, 6)
+        print('Выпало:', n2)
+        # Определение результата (3 возможных варианта)
+        if n1 > n2:
+            print('\tВыиграл', igrok1)
+            countPlayer01 += 1
+        elif n1 < n2:
+            print('\tВыиграл', igrok2)
+            countPlayer02 += 1
+        else:
+            print('\tНичья')
+        return countPlayer01, countPlayer02
 
+
+countPlayer01, countPlayer02 = corGames()
 # ЗАДАЧА 02:  Добавьте реализацию определения победителя. Например, с помощью
 # подсчета количества побед каждым игроком, или суммированием очков.
 # ЗАДАЧА 03:  После цикла добавьте оператор ветвления для определения итогового
 # победителя.
 
-print('Итог счета: (', igrok1, '/', igrok2, ') - ', countPlaer01, ':', countPlaer02, sep='')
-if countPlaer01 > countPlaer02:
-    print('По итогу сражения одержал победу: ', igrok1)
-elif countPlaer01 < countPlaer02:
-    print('По итогу сражения одержал победу: ', igrok2)
-else:
-    print('Ничья')
+print('Итог счета: (', igrok1, '/', igrok2, ') - ', countPlayer01, ':', countPlayer02, sep='')
+
+
+def result(countPlayer01, countPlayer02, textVIN):
+    if countPlayer01 > countPlayer02:
+        print(textVIN, igrok1)
+    elif countPlayer01 < countPlayer02:
+        print(textVIN, igrok2)
+    else:
+        print('Ничья')
+
+
+result(countPlayer01, countPlayer02, 'По итогу сражения одержал победу:')
