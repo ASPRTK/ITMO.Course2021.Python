0b5a397d1080512493086c02f893546262e1ea9e
Георгий Исаев <georgiyelbaf@gmail.com>
2021-12-25 14:58:29 +0300

    FT - menu

---
+++ FinalTask/main.py
@@ -0,0 +1,31 @@
+
+
+def main():
+    while(True):
+        print("")
+        print("1-Добавить")
+        print("2-Показать все")
+        print("3-Показать по дате")
+        print("4-Показать по категории")
+        print("5-Показать по min->max")
+        print("6-Удалить запись")
+        print("0-Завершить работу")
+
+        valueMenu = input('Введите номер меню: ')
+        if valueMenu == "1":
+            print("Выбрано 1-Добавить")
+        elif valueMenu == "2":
+            print("Выбрано 2-Показать все")
+        elif valueMenu == "3":
+            print("Выбрано 3-Показать по дате")
+        elif valueMenu == "4":
+            print("Выбрано 4-Показать по категории")
+        elif valueMenu == "5":
+            print("Выбрано 5-Показать по min->max")
+        elif valueMenu == "6":
+            print("Выбрано 6-Удалить запись")
+        elif valueMenu == "0":
+            break
+            print("Работа приложения завершена")
+
+main()
