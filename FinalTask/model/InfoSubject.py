8801e33cdd3443a677ea42e421322377bbb919b7
Георгий Исаев <georgiyelbaf@gmail.com>
2021-12-27 00:16:50 +0300

    fix исправлено отображение и запись

--- FinalTask/model/InfoSubject.py
+++ FinalTask/model/InfoSubject.py
@@ -2,10 +2,13 @@
 class InfoSubject(object):
     def __init__(self, category, product, cost, dateBuy):
         """Constructor"""
-        self.category = category
-        self.product = product
+        self.category = category.capitalize()
+        self.product = product.capitalize()
         self.cost = cost
         self.dateBuy = dateBuy
+
+
+
     def __str__(self):
         string = "Category: " + self.category + "\n"
         string += "Product: " + self.product + "\n"
